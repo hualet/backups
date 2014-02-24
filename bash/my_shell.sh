0bc08c25c4071258b539b6a1d2dcdd62c651ef7c
Hualet Wang <mr.asianwang@gmail.com>
2014-02-24 15:24:12 +0800

    rm HyperSpec, add gitconfig

--- bash/my_shell.sh
+++ bash/my_shell.sh
@@ -3,13 +3,14 @@
 export PROJECT_HOME=~/project
 export MY_BIN_HOME=~/bin
 export JAVA_HOME=/usr/share/jdk1.7.0_40/
-export PATH=/opt/Qt5.1.1/5.1.1/gcc/bin:${PATH}:${JAVA_HOME}/bin:${MY_BIN_HOME}/dex2jar-0.0.9.15:${MY_BIN_HOME}/jd-gui-linux
+export PATH=/opt/Qt5.2.0/5.2.0/gcc/bin:${PATH}:${JAVA_HOME}/bin:${MY_BIN_HOME}/dex2jar-0.0.9.15:${MY_BIN_HOME}/jd-gui-linux
 export CLASSPATH=.:${JAVA_HOME}/lib:${JAVA_HOME}/jre/lib
 
 alias ack='ack-grep'
 alias cscope_build_c='find . -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" > cscope.files;
                     cscope -q -R -b -i cscope.files'
 alias ec='emacsclient'
+alias openhere='xdg-open .'
 alias vpn='python ~/goagent/local/proxy.py'
 alias ssh_proxy='ssh -qTfnN -D 7070 hualet@vip04.gossh.net -p 443'
 alias vps_proxy='ssh -qTfnN -D 7070 hualet@192.157.206.233'
@@ -19,3 +20,7 @@ alias dss2014='~/project/dss/frame/main.py'
 
 alias hualet2deepin='cp ~/.emacs ~/Backup/hualet-emacs/.emacs ; cp ~/Backup/deepin-emacs/.emacs ~/.emacs'
 alias deepin2hualet='cp ~/.emacs ~/Backup/deepin-emacs/.emacs ; cp ~/Backup/hualet-emacs/.emacs ~/.emacs'
+
+
+# Settings
+ulimit -c unlimited
