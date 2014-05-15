d90db23715e6329db4eee8d0d11c273c469af3c5
Hualet Wang <mr.asianwang@gmail.com>
2014-05-15 13:08:42 +0800

    update

--- auto-install-software.sh
+++ auto-install-software.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install git zsh w3m cowsay stardict sdcv pyflakes imagemagick emacs24 gettext-el mp3info vorbis-tools \
+sudo apt-get install git ipython zsh w3m cowsay stardict sdcv pyflakes imagemagick emacs24 gettext-el mp3info vorbis-tools \
     cscope cscope-el \
     slime clisp cl-quicklisp \
     golang-mode \
