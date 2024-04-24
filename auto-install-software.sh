f80f9078d16e8d2c4b13b3353104cb453c681e6d
Hualet Wang <hualet@hualet.org>
2024-04-24 21:19:35 +0800

    update auto install clis

--- auto-install-software.sh
+++ auto-install-software.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install git \
-    ipython build-essential\
+    ipython3 build-essential\
     d-feet \
-    zsh suckless-tools\
-    trash-cli \
-    w3m \
-    ack-grep \
-    cowsay \
-    stardict \
-    sdcv \
-    pyflakes \
-    imagemagick \
-    emacs24 gettext-el mp3info vorbis-tools cscope cscope-el \
-    slime clisp cl-quicklisp \
-    python-epc python-jedi \
-    guake kupfer autojump\
+    zsh suckless-tools
